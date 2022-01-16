f6636d31bb110b646cd88f7335db9fd1ceb7ea72
alquimiaec <97817189+alquimiaec@users.noreply.github.com>
2022-01-15 22:38:08 -0500

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -1,1 +1,10 @@
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
 
+authenticate = tweepy.OAutHandler(consumer_key, consumer_secret)
+
+authenticate.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(authenticate, wait_on_rate_limit = True)
